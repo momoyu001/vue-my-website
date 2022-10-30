98b438116dc0d5f24afde1a2119940722ac9536d
momoyu001 <lixia1220x@163.com>
2022-10-30 11:06:00 +0800

    feat: 搭建基本的页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <RouterLink to="/">Home</RouterLink>
-  <RouterView />
+  <div class="main-app-module">
+    <div class="app-top"></div>
+    <div class="app-main">
+      <RouterView />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 </script>
 
-<style scoped></style>
+<style scoped>
+.main-app-module {
+  height: 100%;
+  min-width: 800px;
+}
+
+.app-top {
+  height: 100px;
+}
+
+.app-main {
+  height: calc(100% - 100px);
+}
+</style>
