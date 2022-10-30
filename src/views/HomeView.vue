c130768a3a75d23e10c31c57ba4ae485fd869aa7
momoyu001 <lixia1220x@163.com>
2022-10-30 11:11:05 +0800

    feat: 调整首页

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,24 @@
-<template>首页</template>
+<template>
+  <div class="home-page-module">
+    <div class="home-text">
+      <div>一起搞前端~~~</div>
+      <div>冲冲冲</div>
+    </div>
+    <div>摸鱼的同时不要忘了搞xio习哟！！！</div>
+  </div>
+</template>
 
 <script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.home-page-module {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  .home-text {
+    padding-top: 120px;
+    font-size: 28px;
+    color: #42b883;
+  }
+}
+</style>
