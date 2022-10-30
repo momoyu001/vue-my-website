1af1a8cacf87a77eb8a94f3f622906ee3b0faa84
momoyu001 <lixia1220x@163.com>
2022-10-30 11:09:27 +0800

    feat: 因配置 markdown 的 style 为 github 之后，没有自动引入对应的样式，手动添加一个 github-markdown.css 文件

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
       },
       builders: [
         code({
-          theme: "solarizedLight", // 代码块高亮样式
+          theme: "base", // 代码块高亮样式
         }),
       ],
     }),
